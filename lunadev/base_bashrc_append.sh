25c0b4ab725d93f3d3b369474b3fa4c2def590cf
Manglemix <shabouza030@gmail.com>
2023-08-29 00:49:11 -0600

    better srcinstall

--- lunadev/base_bashrc_append.sh
+++ lunadev/base_bashrc_append.sh
@@ -1,13 +1,11 @@
 # source humble installation
 source /opt/ros/humble/setup.bash
 # source install
-alias srcinstall='source install/setup.bash'
+alias srcinstall='source /root/lunadev-2024/install/setup.bash'
 # resolve deps, build, then source overlay
 alias colcbuild='rosdep install --from-paths src -y --ignore-src && colcon build --symlink-install && source install/setup.bash'
 # test with output printed to console
 alias colctest='colcon test --return-code-on-test-failure --event-handlers console_cohesion+'
-# list dev paths for usb devices
-# alias findusb='/root/findusbdev.sh'
 # delete all generated files in the current directory
 alias colcclean='
 if [ "$PWD" = "/usr-ws-2023" ]; then
@@ -17,3 +15,7 @@ else
 fi'
 # Run rviz
 alias rviz='ros2 run rviz2 rviz2'
+# Try srcinstall
+if [ -f /root/lunadev-2024/install/setup.bash ]; then
+    source /root/lunadev-2024/install/setup.bash
+fi
