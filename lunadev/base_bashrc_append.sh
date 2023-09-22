ca584cf9170d4c1a01171858cc72da102f4e97f1
Manglemix <shabouza030@gmail.com>
2023-09-21 20:30:23 -0600

    added alias and fixed colcclean

--- lunadev/base_bashrc_append.sh
+++ lunadev/base_bashrc_append.sh
@@ -6,18 +6,16 @@ alias srcinstall='source /root/lunadev-2024/install/setup.bash'
 alias colcbuild='rosdep install --from-paths src -y --ignore-src && colcon build --symlink-install && source install/setup.bash'
 # test with output printed to console
 alias colctest='colcon test --return-code-on-test-failure --event-handlers console_cohesion+'
-# delete all generated files in the current directory
-alias colcclean='
-if [ "$PWD" = "/usr-ws-2023" ]; then
-    rm -rf build install log
-else
-    echo "Not in usr-ws-2023"
-fi'
+# delete all auto-generated files in lunadev
+alias colcclean='rm -rf /root/lunadev-2024/build /root/lunadev-2024/install /root/lunadev-2024/log'
 # Run rviz
 alias rviz='ros2 run rviz2 rviz2'
 
 # Start VNC
 alias startvnc='python3 /root/lunadev-2024/lunadev/start_vnc.py'
+
+# Start telemetry tunnel
+alias starttele='python3 /root/lunadev-2024/lunadev/start_telemetry_tunnel.py'
 
 # Try srcinstall
 if [ -f /root/lunadev-2024/install/setup.bash ]; then
