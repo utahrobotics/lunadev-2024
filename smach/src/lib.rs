4f9df4ef819e66a2f4402caeff4ac58d663efe93
Manglemix <shabouza030@gmail.com>
2023-12-03 22:29:29 -0700

    smach implementation

--- smach/src/lib.rs
+++ smach/src/lib.rs
@@ -1,164 +1,126 @@
-use async_trait::async_trait;
-use std::{
-    future::Future,
-    marker::PhantomData,
-    sync::{Arc, Weak},
-};
-use tokio::sync::{oneshot, Mutex};
+use std::{future::Future, pin::Pin};
 
-// #[async_trait]
-// trait StateFn<B, R>: Send {
-//     async fn call(&mut self, blackboard: &mut B) -> StateEnded<B, R>;
-// }
+use tokio::sync::{mpsc, broadcast};
 
-// struct SendSyncPhantom<T>(PhantomData<fn(T)>);
 
-// // unsafe impl<T> Send for SendSyncPhantom<T> {}
-// // unsafe impl<T> Sync for SendSyncPhantom<T> {}
-
-// impl<T> Default for SendSyncPhantom<T> {
-//     fn default() -> Self {
-//         Self(PhantomData)
+// impl<'a, B: AsMut<T>, T> FromBlackboard<'a, B> for &'a mut T {
+//     fn from_bb(bb: &'a mut B) -> Self {
+//         bb.as_mut()
 //     }
 // }
 
-// #[async_trait]
-// impl<'a, B, SB, R, F, Fut> StateFn<B, R> for (F, SendSyncPhantom<SB>)
-// where
-//     SB: 'a,
-//     B: AsSubBlackboard<'a, SB> + Send,
-//     F: FnMut(SB) -> Fut + Send + 'static,
-//     Fut: Future<Output = StateEnded<B, R>> + Send,
-// {
-//     async fn call(&mut self, blackboard: &mut B) -> StateEnded<B, R> {
-//         (self.0)(blackboard.as_sub()).await
-//     }
-// }
 
-enum StateEndedEnum<B, R> {
-    NextState(Arc<Mutex<Box<dyn StateFn<B, R>>>>),
-    End(R),
+pub struct TransitionInit<B> {
+    sender: mpsc::Sender<B>,
+    alive_recv: broadcast::Receiver<()>
 }
 
-pub struct StateEnded<B, R>(StateEndedEnum<B, R>);
 
-impl<B, R> StateEnded<B, R> {
-    pub fn transition(next_state: State<B, R>) -> Self {
-        Self(StateEndedEnum::NextState(next_state.func))
-    }
-    pub fn end(return_val: R) -> Self {
-        Self(StateEndedEnum::End(return_val))
-    }
+pub struct Transitioned {
+    fut: Option<Pin<Box<dyn Future<Output = ()> + Send>>>,
 }
 
-pub trait AsSubBlackboard<'a, SB: 'a> {
-    fn as_sub(&'a mut self) -> SB;
-}
 
-impl<'a, T> AsSubBlackboard<'a, ()> for T {
-    fn as_sub(&mut self) -> () {
-        ()
-    }
-}
-
-pub struct State<B, R> {
-    func: Arc<Mutex<Box<dyn StateFn<B, R>>>>,
-}
-
-impl<B, R> Clone for State<B, R> {
-    fn clone(&self) -> Self {
-        Self {
-            func: self.func.clone(),
-        }
-    }
-}
-
-impl<B, R> State<B, R> {
-    pub fn new() -> Self
+impl<B> TransitionInit<B> {
+    pub fn next_state<T, R, Fut>(mut self, blackboard: B, state: fn(B) -> Fut) -> Transitioned
     where
-        B: Send,
+        B: Send + 'static,
+        Fut: Future<Output=(B, R)> + Send + 'static,
+        T: Transition<R, B>
     {
-        Self {
-            func: Arc::new(Mutex::new(Box::new((
-                |_| async { unimplemented!() },
-                SendSyncPhantom::default(),
-            )))),
-        }
-    }
+        Transitioned {
+            fut: Some(Box::pin(async move {
+                let (blackboard, output) = state(blackboard).await;
 
-    pub fn provide_fn<SB, F, Fut>(&mut self, f: F)
-    where
-        SB: 'static,
-        B: AsSubBlackboard<'static, SB> + Send,
-        F: FnMut(SB) -> Fut + Send + 'static,
-        Fut: Future<Output = StateEnded<B, R>> + Send,
-    {
-        self.func = Arc::new(Mutex::new(Box::new((f, SendSyncPhantom::default()))));
-    }
-
-    pub async fn run(&mut self, blackboard: &mut B) -> R {
-        match self.func.lock().await.call(blackboard).await.0 {
-            StateEndedEnum::NextState(mut current_state) => loop {
-                let next_state;
-                match current_state.lock().await.call(blackboard).await.0 {
-                    StateEndedEnum::NextState(x) => next_state = x,
-                    StateEndedEnum::End(x) => break x,
+                let trans = T::transition(output, blackboard, TransitionInit { sender: self.sender.clone(), alive_recv: self.alive_recv.resubscribe() });
+                let Some(fut) = trans.fut else {
+                    return;
                 };
-                current_state = next_state;
-            },
+                tokio::spawn(async move {
+                    tokio::select! {
+                        _ = self.alive_recv.recv() => return,
+                        () = fut => { }
+                    }
+                });
+            }))
+        }
+    }
 
-            StateEndedEnum::End(x) => x,
+    pub fn exit_machine(self, blackboard: B) -> Transitioned {
+        self.sender.try_send(blackboard).unwrap();
+        Transitioned {
+            fut: None
         }
     }
 }
+
+
+pub trait Transition<T, B> {
+    fn transition(input: T, blackboard: B, init: TransitionInit<B>) -> Transitioned;
+}
+
+
+pub async fn start_machine<T, B, R, Fut>(blackboard: B, init_state: fn(B) -> Fut) -> B
+where
+    B: Send + 'static,
+    Fut: Future<Output=(B, R)> + Send,
+    T: Transition<R, B>
+{
+    let (blackboard, output) = init_state(blackboard).await;
+    let (sender, mut receiver) = mpsc::channel(1);
+    let (_alive_sender, alive_recv) = broadcast::channel(1);
+
+    let trans = T::transition(output, blackboard, TransitionInit { sender, alive_recv });
+    let Some(fut) = trans.fut else {
+        return receiver.recv().await.expect("State has panicked");
+    };
+    fut.await;
+    receiver.recv().await.expect("State has panicked")
+}
+    
 
 #[cfg(test)]
 mod tests {
-    use crate::{AsSubBlackboard, State, StateEnded};
+    use crate::{start_machine, Transition};
 
-    struct SumBlackBoard {
-        sum: usize,
+    struct SumBlackboard {
+        sum: usize
     }
 
-    struct SubSumBlackBoard<'a> {
-        sum: &'a mut usize,
-    }
+    struct SumTransition;
 
-    impl<'a> AsSubBlackboard<'a, SubSumBlackBoard<'a>> for SumBlackBoard {
-        fn as_sub(&'a mut self) -> SubSumBlackBoard<'a> {
-            SubSumBlackBoard { sum: &mut self.sum }
+    impl Transition<(), SumBlackboard> for SumTransition {
+        fn transition(_: (), blackboard: SumBlackboard, init: crate::TransitionInit<SumBlackboard>) -> crate::Transitioned {
+            init.next_state::<CheckTransition, _, _>(blackboard, check_state)
         }
     }
 
+    struct CheckTransition;
+
+    impl Transition<bool, SumBlackboard> for CheckTransition {
+        fn transition(input: bool, blackboard: SumBlackboard, init: crate::TransitionInit<SumBlackboard>) -> crate::Transitioned {
+            if input {
+                init.exit_machine(blackboard)
+            } else {
+                init.next_state::<SumTransition, _, _>(blackboard, sum_state)
+            }
+        }
+    }
+
+    async fn sum_state(mut bb: SumBlackboard) -> (SumBlackboard, ()) {
+        bb.sum += 1;
+        (bb, ())
+    }
+
+    async fn check_state(bb: SumBlackboard) -> (SumBlackboard, bool) {
+        let finished = bb.sum >= 10;
+        (bb, finished)
+    }
+
     #[tokio::test]
-    async fn add_one() {
-        let mut adder = State::<SumBlackBoard, usize>::new();
-        let mut checker = State::<SumBlackBoard, usize>::new();
-        let adder2 = adder.clone();
-
-        checker.provide_fn(move |bb: SubSumBlackBoard| {
-            let adder = adder2.clone();
-            async move {
-                if *bb.sum == 20 {
-                    StateEnded::end(*bb.sum)
-                } else {
-                    StateEnded::transition(adder.clone())
-                }
-            }
-        });
-
-        let checker2 = checker.clone();
-        adder.provide_fn(move |bb: SubSumBlackBoard| {
-            let checker = checker2.clone();
-            async {
-                *bb.sum += 1;
-                StateEnded::transition(checker)
-            }
-        });
-
-        let mut blackboard = SumBlackBoard { sum: 0 };
-        let returned = checker.run(&mut blackboard).await;
-        assert_eq!(returned, blackboard.sum);
-        assert_eq!(returned, 20);
+    async fn test_adder01() {
+        let mut bb = SumBlackboard { sum: 0 };
+        bb = start_machine::<SumTransition, _, _, _>(bb, sum_state).await;
+        assert_eq!(bb.sum, 10);
     }
 }
