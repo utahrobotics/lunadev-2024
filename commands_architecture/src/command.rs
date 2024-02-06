2152673ea16d96fafada845debdbc8319ab03369
Micah Ribbens <mdribbens22@outlook.com>
2024-02-06 10:26:31 -0700

    Minor changes to Command trait

--- commands_architecture/src/command.rs
+++ commands_architecture/src/command.rs
@@ -1,8 +1,8 @@
 /// A implied abstract class that all commands must implement. Each command must define the four methods below to execute
 /// their functionality
 pub trait Command {
-    fn start(&self) {}
-    fn run_every_cycle(&self) {}
-    fn has_finished(&self) -> bool { return false; }
-    fn finish(&self) {}
+    fn start(&self);
+    fn run_every_cycle(&self);
+    fn has_finished(&self) -> bool;
+    fn finish(&self);
 }
