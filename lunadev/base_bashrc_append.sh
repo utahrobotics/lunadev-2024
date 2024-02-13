9626f827d8d557353be2ab8c79f56375f4b92c72
Manglemix <shabouza030@gmail.com>
2024-02-13 15:25:59 -0700

    deprecating ssh tunnel files

--- lunadev/base_bashrc_append.sh
+++
@@ -1,8 +0,0 @@
-# Start VNC
-alias startvnc='python3 /root/lunadev-2024/lunadev/start_vnc.py'
-
-# Start telemetry tunnel
-alias starttele='python3 /root/lunadev-2024/lunadev/start_telemetry_tunnel.py'
-
-# Tool for resetting a USB device
-alias resetusb='python3 /root/lunadev-2024/lunadev/reset_usb.py'
