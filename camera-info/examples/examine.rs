e3fcaa1ff17364ddf4845e0c36a0a26f9cc6f4f6
Manglemix <shabouza030@gmail.com>
2024-03-18 02:07:25 -0600

    finishing up examiner

--- camera-info/examples/examine.rs
+++ camera-info/examples/examine.rs
@@ -3,10 +3,10 @@ use std::io::stdin;
 use camera::{discover_all_cameras, Camera};
 use camera_info::interactive_examine;
 use unros::{
-    anyhow::{self, Context}, tokio::{self, task::JoinHandle}, Application
+    anyhow::{self, Context},
+    tokio::{self, task::JoinHandle},
+    Application,
 };
-
-
 
 #[unros::main]
 async fn main(mut app: Application) -> anyhow::Result<Application> {
@@ -25,18 +25,23 @@ async fn main(mut app: Application) -> anyhow::Result<Application> {
                 input.clear();
                 continue;
             };
-        
+
             break index;
         };
 
         Ok(index)
     });
-    
+
     let index = join.await.unwrap()?;
 
     let mut camera = Camera::new(index)?;
     let camera_name = camera.get_camera_name().to_string();
-    interactive_examine(&mut app, |x| camera.accept_image_received_sub(x), camera_name).await;
+    interactive_examine(
+        &mut app,
+        |x| camera.accept_image_received_sub(x),
+        camera_name,
+    )
+    .await;
     app.add_node(camera);
 
     Ok(app)
--- camera-info/src/lib.rs
+++ camera-info/src/lib.rs
@@ -1,14 +1,36 @@
 use std::{
-    array, collections::HashMap, hash::{BuildHasherDefault, DefaultHasher}, io::stdin, num::NonZeroUsize, sync::{Arc, OnceLock}, time::Duration
+    array,
+    collections::HashMap,
+    hash::{BuildHasherDefault, DefaultHasher},
+    io::stdin,
+    num::NonZeroUsize,
+    path::Path,
+    sync::{Arc, OnceLock},
+    time::Duration,
 };
 
 use apriltag::AprilTagDetector;
 use image::DynamicImage;
-use opencv::{calib3d::{calibrate_camera, find_chessboard_corners, get_optimal_new_camera_matrix, CALIB_CB_ADAPTIVE_THRESH, CALIB_CB_NORMALIZE_IMAGE}, core::{Mat, MatTraitConst, Point3f, Rect, Size, TermCriteria, Vector}, imgproc::corner_sub_pix, types::{VectorOfMat, VectorOfPoint2f, VectorOfPoint3f, VectorOfVec3d}};
+use opencv::{
+    calib3d::{
+        calibrate_camera, find_chessboard_corners, get_optimal_new_camera_matrix,
+        CALIB_CB_ADAPTIVE_THRESH, CALIB_CB_NORMALIZE_IMAGE,
+    },
+    core::{Mat, MatTraitConst, Point3f, Rect, Size, TermCriteria, Vector},
+    imgproc::corner_sub_pix,
+    types::{VectorOfMat, VectorOfPoint2f, VectorOfPoint3f, VectorOfVec3d},
+};
 use rig::Robot;
 use serde::{Deserialize, Serialize};
-use serde_json::{from_reader, to_string_pretty};
-use unros::{anyhow, log, pubsub::{Publisher, Subscriber, Subscription}, setup_logging, tokio::{self, task::JoinHandle}, Application};
+use serde_json::{from_reader, to_string_pretty, to_writer_pretty};
+use unros::{
+    anyhow::{self, Context},
+    log,
+    pubsub::{Publisher, Subscriber, Subscription},
+    setup_logging,
+    tokio::{self, task::JoinHandle},
+    Application,
+};
 
 static CAMERA_DB: OnceLock<HashMap<String, Arc<CameraInfo>>> = OnceLock::new();
 
@@ -55,7 +77,6 @@ fn get_camera_db() -> &'static HashMap<String, Arc<CameraInfo>> {
     })
 }
 
-
 #[derive(Serialize, Deserialize)]
 struct DistortionData {
     distortion_coefficients: Vec<f64>,
@@ -79,7 +100,7 @@ pub struct CameraInfo {
     pub focal_length_px: Option<NonZeroUsize>,
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(default)]
-    distortion_data: Option<DistortionData>
+    distortion_data: Option<DistortionData>,
 }
 
 impl CameraInfo {
@@ -95,19 +116,30 @@ impl CameraInfo {
     }
 }
 
+impl Drop for CameraInfo {
+    fn drop(&mut self) {
+        let display = to_string_pretty(self).unwrap();
+        println!("{display}");
+    }
+}
+
 struct FocalLengthEstimate {
     tag_distance: f64,
     width: f64,
-    id: usize
+    id: usize,
 }
 
-pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(Subscription<Arc<DynamicImage>>), camera_name: String) {
+pub async fn interactive_examine(
+    app: &mut Application,
+    accept_sub: impl FnOnce(Subscription<Arc<DynamicImage>>),
+    camera_name: String,
+) {
     let join: JoinHandle<Result<_, anyhow::Error>> = tokio::task::spawn_blocking(|| {
         let stdin = stdin();
         let mut input = String::new();
-    
+
         let estimate_fps;
-    
+
         loop {
             println!("Estimate fps (Y/N)?");
             input.clear();
@@ -121,9 +153,9 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
             }
             break;
         }
-    
+
         let chessboard;
-    
+
         loop {
             println!("Will you be displaying a chessboard (Y/N)?");
             input.clear();
@@ -137,7 +169,7 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
         }
 
         let focal_length_estimate;
-    
+
         loop {
             println!("Estimate focal length (Y/N)?");
             input.clear();
@@ -191,11 +223,13 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
 
         Ok((chessboard, estimate_fps, focal_length_estimate))
     });
-    
+
     let (chessboard, estimate_fps, focal_length_estimate) = join.await.unwrap().unwrap();
-    
+
     let rig: Robot = toml::from_str(include_str!("lunabot.toml")).unwrap();
-    let (mut elements, _) = rig.destructure::<BuildHasherDefault<DefaultHasher>>(["camera"]).unwrap();
+    let (mut elements, _) = rig
+        .destructure::<BuildHasherDefault<DefaultHasher>>(["camera"])
+        .unwrap();
     let camera_element = elements.remove("camera").unwrap();
 
     let mut camera_sub = Subscriber::new(1);
@@ -209,7 +243,13 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                 return Err(anyhow::anyhow!("Camera did not produce any frames!"));
             };
 
-            let mut camera_info = CameraInfo { width: img.width(), height: img.height(), fps: None, focal_length_px: None, distortion_data: None };
+            let mut camera_info = CameraInfo {
+                width: img.width(),
+                height: img.height(),
+                fps: None,
+                focal_length_px: None,
+                distortion_data: None,
+            };
 
             if chessboard {
                 let mut object_point = VectorOfPoint3f::new();
@@ -220,9 +260,10 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                 }
                 let mut object_points = Vector::<VectorOfPoint3f>::new();
                 let mut image_points = Vector::<VectorOfPoint2f>::new();
-                
+
                 let img_size = Size::new(img.width() as i32, img.height() as i32);
-                let criteria = TermCriteria::default().expect("Failed to generate default TermCriteria");
+                let criteria =
+                    TermCriteria::default().expect("Failed to generate default TermCriteria");
 
                 for iteration in 0..10 {
                     println!("{iteration}: Finding chessboard corners");
@@ -231,35 +272,74 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                             return Err(anyhow::anyhow!("Camera did not produce any frames!"));
                         };
                         let img = img.to_luma8();
-                        let img = Mat::from_slice_rows_cols(&img, img.height() as usize, img.width() as usize).expect("Image should have been converted into a matrix");
-        
+                        let img = Mat::from_slice_rows_cols(
+                            &img,
+                            img.height() as usize,
+                            img.width() as usize,
+                        )
+                        .expect("Image should have been converted into a matrix");
+
                         let mut corners = VectorOfPoint2f::new();
-                        let success = find_chessboard_corners(&img, Size::new(7, 6), &mut corners, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE).expect("Failed to execute find_chessboard_corners");
-        
+                        let success = find_chessboard_corners(
+                            &img,
+                            Size::new(7, 6),
+                            &mut corners,
+                            CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE,
+                        )
+                        .expect("Failed to execute find_chessboard_corners");
+
                         if !success {
                             continue;
                         }
-        
+
                         println!("{iteration}: Refining corners");
-                        corner_sub_pix(&img, &mut corners, Size::new(11, 11), Size::new(-1, -1), criteria).expect("Failed to execute corner_sub_pix");
-                        
+                        corner_sub_pix(
+                            &img,
+                            &mut corners,
+                            Size::new(11, 11),
+                            Size::new(-1, -1),
+                            criteria,
+                        )
+                        .expect("Failed to execute corner_sub_pix");
+
                         object_points.push(object_point.clone());
                         image_points.push(corners);
                         break;
                     }
                 }
-                
-                let mut camera_matrix = Mat::from_slice_rows_cols(&[0, 0, 0, 0, 0, 0, 0, 0, 0], 3, 3).unwrap();
+
+                let mut camera_matrix =
+                    Mat::from_slice_rows_cols(&[0, 0, 0, 0, 0, 0, 0, 0, 0], 3, 3).unwrap();
                 let mut dist_coeffs = Vector::<f64>::new();
                 let mut rvecs = VectorOfMat::new();
                 let mut tvecs = VectorOfVec3d::new();
 
                 println!("Calculating distortion");
-                let err = calibrate_camera(&object_points, &image_points, img_size, &mut camera_matrix, &mut dist_coeffs, &mut rvecs, &mut tvecs, 0, criteria).expect("Failed to execute calibrate_camera");
+                let err = calibrate_camera(
+                    &object_points,
+                    &image_points,
+                    img_size,
+                    &mut camera_matrix,
+                    &mut dist_coeffs,
+                    &mut rvecs,
+                    &mut tvecs,
+                    0,
+                    criteria,
+                )
+                .expect("Failed to execute calibrate_camera");
                 println!("RMS re-projection error: {err}");
                 let mut roi = Rect::new(0, 0, img.width() as i32, img.height() as i32);
-                let new_camera_matrix = get_optimal_new_camera_matrix(&camera_matrix, &dist_coeffs, img_size, 1.0, img_size, Some(&mut roi), false).expect("Failed to execute get_optimal_new_camera_matrix");
-                
+                let new_camera_matrix = get_optimal_new_camera_matrix(
+                    &camera_matrix,
+                    &dist_coeffs,
+                    img_size,
+                    1.0,
+                    img_size,
+                    Some(&mut roi),
+                    false,
+                )
+                .expect("Failed to execute get_optimal_new_camera_matrix");
+
                 let distortion_data = DistortionData {
                     distortion_coefficients: dist_coeffs.into_iter().collect(),
                     camera_matrix: array::from_fn(|i| *camera_matrix.at(i as i32).unwrap()),
@@ -273,11 +353,17 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                 camera_info.distortion_data = Some(distortion_data);
             }
 
-            if let Some(FocalLengthEstimate { tag_distance, width, id }) = focal_length_estimate {
+            if let Some(FocalLengthEstimate {
+                tag_distance,
+                width,
+                id,
+            }) = focal_length_estimate
+            {
                 let mut pose_sub = Subscriber::new(1);
                 let mut length = img.width().max(img.height()) as f64 / 2.0;
                 let mut close_enoughs = 0usize;
                 let mut first = true;
+                let mut fails = 0;
 
                 loop {
                     let mut img_pub = Publisher::default();
@@ -296,9 +382,17 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                     if first {
                         first = false;
                         println!("Waiting for initial apriltag observation...");
-                        pose_sub.recv().await;
+                        let img_fut = async {
+                            loop {
+                                img_pub.set(camera_sub.recv().await);
+                            }
+                        };
+                        tokio::select! {
+                            _ = img_fut => unreachable!(),
+                            _ = pose_sub.recv() => {}
+                        }
                     }
-                    info!("Current length: {length:.2}");
+                    println!("Current length: {length:.2}");
 
                     let img_fut = async {
                         loop {
@@ -320,7 +414,12 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                         _ = tokio::time::sleep(Duration::from_secs(3)) => {}
                     }
                     if observations == 0 {
-                        error!("Received no observations!");
+                        println!("Received no observations!");
+                        fails += 1;
+                        if fails == 3 {
+                            println!("Resetting length...");
+                            length = img.width().max(img.height()) as f64 / 2.0;
+                        }
                         continue;
                     }
                     distance /= observations as f64;
@@ -330,8 +429,8 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                         close_enoughs += 1;
 
                         if close_enoughs >= 5 {
-                            info!("Final length: {new_length:.2}");
-                            camera_info.focal_length_px = Some(NonZeroUsize::new(new_length.round() as usize).unwrap());
+                            camera_info.focal_length_px =
+                                Some(NonZeroUsize::new(new_length.round() as usize).unwrap());
                         }
                     } else {
                         close_enoughs = 0;
@@ -367,10 +466,55 @@ pub async fn interactive_examine(app: &mut Application, accept_sub: impl FnOnce(
                 }
             }
 
-            let display = to_string_pretty(&camera_info).unwrap();
-
             println!("Finished examination of: {camera_name}");
-            println!("{display}");
+
+            let join = tokio::task::spawn_blocking(move || {
+                let stdin = stdin();
+                let mut input = String::new();
+
+                loop {
+                    println!("Save file (Y/N)?");
+                    input.clear();
+                    stdin.read_line(&mut input).expect("Failed to read stdin");
+                    match input.to_ascii_lowercase().trim() {
+                        "y" => {
+                            let mut submap = HashMap::with_capacity(1);
+                            submap.insert(camera_name, camera_info);
+
+                            std::fs::DirBuilder::new()
+                                .recursive(true)
+                                .create(DEFAULT_CAMERA_FOLDER)
+                                .with_context(|| {
+                                    format!("{DEFAULT_CAMERA_FOLDER} should be writable")
+                                })
+                                .unwrap();
+
+                            for i in 0.. {
+                                let path =
+                                    Path::new(DEFAULT_CAMERA_FOLDER).join(format!("block{i}.json"));
+                                let file = match std::fs::File::create(&path) {
+                                    Ok(x) => x,
+                                    Err(e) => match e.kind() {
+                                        std::io::ErrorKind::AlreadyExists => todo!(),
+                                        _ => continue,
+                                    },
+                                };
+                                to_writer_pretty(file, &submap)
+                                    .with_context(|| format!("{path:?} should be writable"))
+                                    .unwrap();
+                                break;
+                            }
+
+                            std::mem::forget(submap);
+                        }
+                        "n" => {}
+                        _ => continue,
+                    }
+                    break;
+                }
+            });
+
+            join.await.unwrap();
 
             Ok(())
         },
