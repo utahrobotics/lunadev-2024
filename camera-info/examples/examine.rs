2125ca258195c58887a587f6ebbf30a20d593183
Manglemix <shabouza030@gmail.com>
2024-03-18 00:06:13 -0600

    calib impl

--- camera-info/examples/examine.rs
+++ camera-info/examples/examine.rs
@@ -2,6 +2,7 @@ use std::{io::stdin, num::NonZeroUsize, time::Duration};
 
 use camera::{discover_all_cameras, Camera};
 use camera_info::CameraInfo;
+use opencv::{calib3d::{calibrate_camera, find_chessboard_corners, CALIB_CB_ADAPTIVE_THRESH, CALIB_CB_NORMALIZE_IMAGE}, core::{Mat, Point3d, Size, TermCriteria, Vector}, imgproc::corner_sub_pix, types::{VectorOfMat, VectorOfPoint2d, VectorOfPoint3d, VectorOfVec3d}};
 use serde_json::to_string_pretty;
 use unros::{
     anyhow::{self, Context}, pubsub::Subscriber, setup_logging, tokio::{self, task::JoinHandle}, Application
@@ -74,12 +75,12 @@ async fn main(mut app: Application) -> anyhow::Result<Application> {
     
     let (index, chessboard, estimate_fps) = join.await.unwrap()?;
 
-    println!("Capturing initial photo");
 
     let mut camera = Camera::new(index)?;
     let mut sub = Subscriber::new(1);
     camera.accept_image_received_sub(sub.create_subscription());
     let camera_name = camera.get_camera_name().to_string();
+    println!("Capturing initial photo");
 
     app.add_node(camera);
     app.add_task(
@@ -106,9 +107,7 @@ async fn main(mut app: Application) -> anyhow::Result<Application> {
                     };
                     tokio::select! {
                         e = fut => return Err(e),
-                        _ = tokio::time::sleep(Duration::from_secs(5)) => {
-    
-                        }
+                        _ = tokio::time::sleep(Duration::from_secs(5)) => {}
                     }
                     let fps = (count as f32 / 5.0).round() as usize;
                     if fps == 0 {
@@ -120,7 +119,39 @@ async fn main(mut app: Application) -> anyhow::Result<Application> {
             }
 
             if chessboard {
-                todo!();
+                let img = img.to_luma8();
+                let img_size = Size::new(img.width() as i32, img.height() as i32);
+                let img = Mat::from_slice_rows_cols(&img, img.height() as usize, img.width() as usize).expect("Image should have been converted into a matrix");
+
+                let mut corners = VectorOfPoint2d::new();
+                println!("Finding chessboard corners...");
+                let success = find_chessboard_corners(&img, Size::new(7, 6), &mut corners, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE).expect("Failed to execute find_chessboard_corners");
+
+                if !success {
+                    return Err(anyhow::anyhow!("Failed to find a chessboard!"));
+                }
+
+                println!("Refining corners...");
+                let criteria = TermCriteria::default().expect("Failed to generate default TermCriteria");
+                corner_sub_pix(&img, &mut corners, Size::new(11, 11), Size::new(-1, -1), criteria).expect("Failed to execute corner_sub_pix");
+                
+                let mut object_points = VectorOfPoint3d::new();
+                for y in 0..6 {
+                    for x in 0..7 {
+                        object_points.push(Point3d::new(x as f64, y as f64, 0.0));
+                    }
+                }
+
+                let mut image_points = Vector::<VectorOfPoint2d>::new();
+                image_points.push(corners);
+                let mut camera_matrix = Mat::from_slice_rows_cols(&[0, 0, 0, 0, 0, 0, 0, 0, 0], 3, 3).unwrap();
+                let mut dist_coeffs = Vector::<f64>::new();
+                let mut rvecs = VectorOfMat::new();
+                let mut tvecs = VectorOfVec3d::new();
+
+                println!("Calculating distortion...");
+                let err = calibrate_camera(&object_points, &image_points, img_size, &mut camera_matrix, &mut dist_coeffs, &mut rvecs, &mut tvecs, 0, criteria).expect("Failed to execute calibrate_camera");
+                println!("{err}\n{camera_matrix:?}\n{dist_coeffs:?}");
             }
 
             let display = to_string_pretty(&camera_info).unwrap();
