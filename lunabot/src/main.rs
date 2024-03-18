e1b86514cefc14fe7cef7d075adfd0ec04360a39
Manglemix <shabouza030@gmail.com>
2024-03-18 16:25:09 -0600

    added camera info to lunabot

--- lunabot/src/main.rs
+++ lunabot/src/main.rs
@@ -8,6 +8,7 @@ use std::{
 
 use apriltag::{AprilTagDetector, PoseObservation};
 use camera::{discover_all_cameras, Camera};
+use camera_info::CameraInfo;
 use costmap::local::LocalCostmap;
 use fxhash::FxBuildHasher;
 use localization::{
@@ -68,7 +69,10 @@ async fn main(mut app: Application) -> anyhow::Result<Application> {
         20,
     )
     .await?;
-    camera.accept_image_received_sub(telemetry.create_image_subscription());
+    let camera_info = CameraInfo::from_name(camera.get_camera_name()).unwrap_or_default();
+    camera.accept_image_received_sub(
+        camera_info.undistort_subscription(telemetry.create_image_subscription()),
+    );
 
     // let costmap_ref = costmap.clone();
 
