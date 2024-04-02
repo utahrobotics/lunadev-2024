264b48a65a356e8ee57aa6cb24833aa8b9dfd5e2
Manglemix <shabouza030@gmail.com>
2024-04-01 18:00:55 -0600

    test new camera dep

--- camera/src/lib.rs
+++ camera/src/lib.rs
@@ -6,14 +6,8 @@
 
 use std::sync::Arc;
 
+use eye::hal::{traits::Context as HalContext, PlatformContext};
 use image::{imageops::FilterType, DynamicImage};
-use nokhwa::{
-    pixel_format::RgbFormat,
-    query,
-    utils::{
-        CameraFormat, CameraIndex, FrameFormat, RequestedFormat, RequestedFormatType, Resolution,
-    },
-};
 use unros::{
     anyhow::{self, Context},
     async_trait, asyncify_run, log,
@@ -37,19 +31,19 @@ pub struct Camera {
 impl Camera {
     /// Creates a pending connection to the camera with the given index.
     pub fn new(camera_index: u32) -> anyhow::Result<Self> {
-        let tmp_camera = nokhwa::Camera::new(
-            CameraIndex::Index(camera_index),
-            RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate),
-        )
-        .context("Failed to initialize camera")?;
-        let camera_name = tmp_camera.info().human_name();
-        log::info!("Pinged {} with index {}", camera_name, camera_index);
+        // let tmp_camera = nokhwa::Camera::new(
+        //     CameraIndex::Index(camera_index),
+        //     RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate),
+        // )
+        // .context("Failed to initialize camera")?;
+        // let camera_name = tmp_camera.info().human_name();
+        // log::info!("Pinged {} with index {}", camera_name, camera_index);
         Ok(Self {
             fps: 0,
             res_x: 0,
             res_y: 0,
             camera_index,
-            camera_name,
+            camera_name: todo!(),
             image_received: Default::default(),
             intrinsics: Default::default(),
         })
@@ -75,65 +69,72 @@ impl Node for Camera {
 
     async fn run(mut self, context: RuntimeContext) -> anyhow::Result<()> {
         setup_logging!(context);
+        Ok(())
+        // let index = CameraIndex::Index(self.camera_index);
 
-        let index = CameraIndex::Index(self.camera_index);
+        // let requested = if self.fps > 0 {
+        //     if self.res_x > 0 && self.res_y > 0 {
+        //         RequestedFormat::new::<RgbFormat>(RequestedFormatType::Exact(CameraFormat::new(
+        //             Resolution::new(self.res_x, self.res_y),
+        //             FrameFormat::RAWRGB,
+        //             self.fps,
+        //         )))
+        //     } else {
+        //         RequestedFormat::new::<RgbFormat>(RequestedFormatType::HighestFrameRate(self.fps))
+        //     }
+        // } else if self.res_x > 0 && self.res_y > 0 {
+        //     RequestedFormat::new::<RgbFormat>(RequestedFormatType::HighestResolution(
+        //         Resolution::new(self.res_x, self.res_y),
+        //     ))
+        // } else {
+        //     RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate)
+        // };
 
-        let requested = if self.fps > 0 {
-            if self.res_x > 0 && self.res_y > 0 {
-                RequestedFormat::new::<RgbFormat>(RequestedFormatType::Exact(CameraFormat::new(
-                    Resolution::new(self.res_x, self.res_y),
-                    FrameFormat::RAWRGB,
-                    self.fps,
-                )))
-            } else {
-                RequestedFormat::new::<RgbFormat>(RequestedFormatType::HighestFrameRate(self.fps))
-            }
-        } else if self.res_x > 0 && self.res_y > 0 {
-            RequestedFormat::new::<RgbFormat>(RequestedFormatType::HighestResolution(
-                Resolution::new(self.res_x, self.res_y),
-            ))
-        } else {
-            RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate)
-        };
+        // let res_x = self.res_x;
+        // let res_y = self.res_y;
 
-        let res_x = self.res_x;
-        let res_y = self.res_y;
+        // let drop_check = DropCheck::default();
+        // let drop_obs = drop_check.get_observing();
 
-        let drop_check = DropCheck::default();
-        let drop_obs = drop_check.get_observing();
-
-        asyncify_run(move || {
-            let mut camera =
-                nokhwa::Camera::new(index, requested).context("Failed to initialize camera")?;
-            camera.open_stream()?;
-            loop {
-                let frame = camera.frame()?;
-                if drop_obs.has_dropped() {
-                    break Ok(());
-                }
-                let decoded = frame.decode_image::<RgbFormat>().unwrap();
-                let mut img = DynamicImage::from(decoded);
-                if res_x != 0 && res_y != 0 {
-                    img = img.resize(res_x, res_y, FilterType::CatmullRom);
-                }
-                self.image_received.set(Arc::new(img));
-            }
-        })
-        .await
+        // asyncify_run(move || {
+        //     let mut camera =
+        //         nokhwa::Camera::new(index, requested).context("Failed to initialize camera")?;
+        //     camera.open_stream()?;
+        //     loop {
+        //         let frame = camera.frame()?;
+        //         if drop_obs.has_dropped() {
+        //             break Ok(());
+        //         }
+        //         let decoded = frame.decode_image::<RgbFormat>().unwrap();
+        //         let mut img = DynamicImage::from(decoded);
+        //         if res_x != 0 && res_y != 0 {
+        //             img = img.resize(res_x, res_y, FilterType::CatmullRom);
+        //         }
+        //         self.image_received.set(Arc::new(img));
+        //     }
+        // })
+        // .await
     }
 }
 
 /// Returns an iterator over all the cameras that were identified on this computer.
 pub fn discover_all_cameras() -> anyhow::Result<impl Iterator<Item = Camera>> {
-    Ok(query(nokhwa::utils::ApiBackend::Auto)?
-        .into_iter()
-        .filter_map(|info| {
-            let CameraIndex::Index(n) = info.index() else {
-                return None;
-            };
-            let Ok(cam) = Camera::new(*n) else {
-                return None;
-            };
-            Some(cam)
-        }))
+    let ctx = PlatformContext::all().next().ok_or_else(|| anyhow::anyhow!("Unable to get PlatformContext"))?;
+    let dev_descrs = ctx.devices()?;
+    println!("{dev_descrs:?}");
+    // Ok(query(nokhwa::utils::ApiBackend::Auto)?
+    //     .into_iter()
+    //     .filter_map(|info| {
+    //         let CameraIndex::Index(n) = info.index() else {
+    //             return None;
+    //         };
+    //         match Camera::new(*n) {
+    //             Ok(cam) => Some(cam),
+    //             Err(_) => {
+    //                 None
+    //             }
+    //         }
+    //     }))
+
+    Ok(std::iter::empty())
 }
