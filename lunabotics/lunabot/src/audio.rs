df8c25c9e036101e59c0631202ab1fc688ace9c4
Manglemix <shabouza030@gmail.com>
2024-05-07 22:28:24 -0600

    fixes

--- lunabotics/lunabot/src/audio.rs
+++ lunabotics/lunabot/src/audio.rs
@@ -8,17 +8,14 @@ static SINK: OnceLock<Box<[Sink]>> = OnceLock::new();
 
 pub fn init_buzz() {
     let mut sinks = vec![];
-    for device in cpal::default_host().output_devices().unwrap() {
-        let (stream, stream_handle) =
-            OutputStream::try_from_device(&device).expect("Failed to open audio stream");
-        std::mem::forget(stream);
-        let sink = Sink::try_new(&stream_handle).unwrap();
-        let source = SineWave::new(300.0);
-        sink.append(source);
-        sink.pause();
-        sinks.push(sink);
-        log::info!("Audio device: {}", device.name().unwrap());
-    }
+    let (stream, stream_handle) =
+        OutputStream::try_default().expect("Failed to open audio stream");
+    std::mem::forget(stream);
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    let source = SineWave::new(300.0);
+    sink.append(source);
+    sink.pause();
+    sinks.push(sink);
     let Ok(()) = SINK.set(sinks.into_boxed_slice()) else {
         unreachable!();
     };
